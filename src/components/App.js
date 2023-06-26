57d19101b78deca4d20ae7925fba8e35d2979b9f
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-06-25 19:28:17 -0700

    Render pizza to the page.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import PizzaForm from "./PizzaForm";
 import PizzaList from "./PizzaList";
 
 function App() {
+  const [pizzaList, setPizzaList] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/pizzas`)
+      .then((r) => r.json())
+      .then((data) => setPizzaList(data));
+  }, []);
+
   return (
     <>
       <Header />
       <PizzaForm />
-      <PizzaList />
+      <PizzaList pizzaList={pizzaList} />
     </>
   );
 }
